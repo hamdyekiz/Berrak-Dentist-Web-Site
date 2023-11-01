685a3c0a110c6fcc53f0a0a66d24c115e921a270
Omer <kurtlarsen31@gmail.com>
2023-11-01 21:07:33 +0300

    Verification code sent

--- appoinment_request.js
+++ appoinment_request.js
@@ -30,10 +30,13 @@ app.listen(port, () => {
 });
 
 
+let verificationCode;
+let name, surname, telNo, email, availableHours;
+
 app.post('/submit', (req, res) => {
     // Access form inputs using req.body
     //const formData = req.body;
-    const {name, surname, telNo, email, availableHours} = req.body;
+    ({name, surname, telNo, email, availableHours} = req.body);
   
     // alınan veriler doğru mu diye kontrol etme
     console.log("name: " + name)
@@ -45,14 +48,80 @@ app.post('/submit', (req, res) => {
     // Send a response or redirect to another page
     res.render("submit.ejs", { name: name, surname: surname, telNo: telNo, email: email, availableHours: availableHours })
 
-    
+    sendVerificationCode(name, surname, email);
+
+
     // Alınan verilerle e posta gönderme
-    sendEmail(name, surname, telNo, email, availableHours);
-
-
+    //sendEmail(name, surname, telNo, email, availableHours);
 
 
 });
+
+
+//e mail'e gönderilen kod verify edilecek.
+app.post('/verify', (req, res) => {
+
+  let enteredCode = req.body;
+  if(verificationCode == enteredCode){
+    sendEmail(name, surname, telNo, email, availableHours);
+  }
+  else{
+    console.log("Wrong code");
+  }
+
+});
+
+
+
+function sendVerificationCode(name, surname, email){
+
+
+  // let randomNumber = Math.floor(Math.random() * 100000);
+
+  // verificationCode = randomNumber.toString().padStart(5, '0');  
+  
+  const min = 10000; // Smallest 5-digit number
+  const max = 99999; // Largest 5-digit number
+  const verificationCode = Math.floor(Math.random() * (max - min + 1) + min);  
+  
+  const transporter = nodemailer.createTransport({
+      host: 'smtp.office365.com',  // Outlook SMTP server
+      port: 587,                  // Port for TLS
+      secure: false,              // true for 465, false for other ports
+      auth: {
+          user: process.env.EMAIL_USER,     // Access email from environment variable
+          pass: process.env.EMAIL_PASSWORD
+      }
+    });
+    // Burada kayıtlı eposta hesabı ve şifresinin admin'den sitede alınıp database'e güvenli bir şekilde kaydedilmesi gerekiyor. 
+
+    const emailContent = `
+    <p>Sayın ${name} ${surname},</p>
+    <p>Berrak diş hekimliğinden aldığınız randevusu için onay kodunuz:</p>
+    <p style="font-size: 30px; font-weight: bold;">${verificationCode}</p>
+  `;
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,   // Access sender email
+      to: email,     // Access recipient email
+      subject: 'Doğrulama Kodu',
+      html: emailContent
+    };
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Email could not be sent:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+   });
+   
+   //return verificationCode;
+}
+
+
+
 
 
 function sendEmail(name, surname, telNo, email, availableHours){

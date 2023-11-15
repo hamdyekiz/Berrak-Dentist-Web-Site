97d2bb389faf3e25e2a1cfd81a62ab2d0e8e44d8
Omer <kurtlarsen31@gmail.com>
2023-11-15 21:38:48 +0300

    proper unit testing for sendEmail, sendVerificationCode, isEmailValid functions

--- appoinment_request.js
+++ appoinment_request.js
@@ -1,111 +1,93 @@
-import express from "express";
-import bodyParser from "body-parser";
-import nodemailer from "nodemailer"
-import dotenv from 'dotenv';
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+const path = require("path");
+const { URL } = require("url");
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const dirName = path.dirname(require.main.filename);
 const app = express();
 const port = 3000;
 
-
 dotenv.config();
-
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(dirName + '/public'));
 
 // Routes
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(dirName + "/public/index.html");
 });
 
 // Start server
-app.listen(port, () => {
+let server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
 
 let verificationCode;
 let name, surname, telNo, email, availableHours;
 
 app.post('/submit', (req, res) => {
     // Access form inputs using req.body
-    //const formData = req.body;
-    ({name, surname, telNo, email, availableHours} = req.body);
-  
+    ({ name, surname, telNo, email, availableHours } = req.body);
+
     // alınan veriler doğru mu diye kontrol etme
-    console.log("name: " + name)
-    console.log("surname: " + surname)
-    console.log("telNo: " + telNo)
-    console.log("email: " + email)
-    console.log("availableHours: " + availableHours)
+    console.log("name: " + name);
+    console.log("surname: " + surname);
+    console.log("telNo: " + telNo);
+    console.log("email: " + email);
+    console.log("availableHours: " + availableHours);
 
     // check if the email is a valid email
-    if(!isEmailValid(email)) {
-      console.log("email is not valid!");
+    if (!isEmailValid(email)) {
+        console.log("email is not valid!");
 
-      res.render("index.ejs", {error: "error"}) // this index.ejs is a dynamic page 
-      //because it alerts the user about the email adress dynamically.
-      //if the mail is not valid it alerts else it works normally.
-      // {error: "error"} is just for placeholder to figure out if there is an error.
-      // it's value is not used in index.ejs file.
-
-      return; //finish the callback function.
+        res.render("index.ejs", { error: "error" });
+        return;
     }
 
     // Send a response or redirect to another page
-    res.render("submit.ejs", { name: name, surname: surname, telNo: telNo, email: email, availableHours: availableHours })
+    res.render("submit.ejs", { name: name, surname: surname, telNo: telNo, email: email, availableHours: availableHours });
 
     // Hastanın mailine onay kodu yollanır.
     sendVerificationCode(name, surname, email);
-
-
 });
-
 
 //e mail'e gönderilen kod verify edilecek.
 app.post('/verify', (req, res) => {
-  let isCodeCorrect = false;
-  console.log(req.body);
-  let {authCode} = req.body;
-  console.log("girilen code: " + authCode);
-  if(verificationCode == authCode){
-    isCodeCorrect = true;
-    console.log("True code");
-    sendEmail(name, surname, telNo, email, availableHours);
-  }
-  else{
-    console.log("Wrong code");
-  }
-  res.render("submit.ejs", {isCodeCorrect: isCodeCorrect});
-
+    let isCodeCorrect = false;
+    console.log(req.body);
+    let { authCode } = req.body;
+    console.log("girilen code: " + authCode);
+    if (verificationCode == authCode) {
+        isCodeCorrect = true;
+        console.log("True code");
+        sendEmail(name, surname, telNo, email, availableHours);
+    } else {
+        console.log("Wrong code");
+    }
+    res.render("submit.ejs", { isCodeCorrect: isCodeCorrect });
 });
 
+function sendVerificationCode(name, surname, email) {
+    // onay kodu 10000 ile 99999 arasında 5 basamaklı bir sayı
+    const min = 10000; // Smallest 5-digit number
+    const max = 99999; // Largest 5-digit number
+    verificationCode = Math.floor(Math.random() * (max - min + 1) + min).toString();
 
-
-function sendVerificationCode(name, surname, email){
-
- 
-  // onay kodu 10000 ile 99999 arasında 5 basamaklı bir sayı  
-  const min = 10000; // Smallest 5-digit number
-  const max = 99999; // Largest 5-digit number
-  verificationCode = Math.floor(Math.random() * (max - min + 1) + min).toString();  
-  
-  const transporter = nodemailer.createTransport({
-      host: 'smtp.office365.com',  // Outlook SMTP server
-      port: 587,                  // Port for TLS
-      secure: false,              // true for 465, false for other ports
-      auth: {
-          user: process.env.EMAIL_USER,     // Access email from environment variable
-          pass: process.env.EMAIL_PASSWORD
-      }
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
     });
-    
+
     const emailContent = `
     <p>Sayın ${name} ${surname},</p>
     <p>Berrak diş hekimliğinden almak istediğiniz randevu için onay kodunuz:</p>
@@ -115,43 +97,35 @@ function sendVerificationCode(name, surname, email){
     <p> Teşekkür ederiz.</p>`;
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,   // Access sender email
-      to: email,     // Access recipient email
-      subject: 'Doğrulama Kodu',
-      html: emailContent
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Doğrulama Kodu',
+        html: emailContent
     };
 
-
     transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log('Email could not be sent:', error);
-      } else {
-        console.log('Email sent:', info.response);
-      }
-   });
-   
+        if (error) {
+            console.log('Email could not be sent:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
 }
 
-
-
-
-
-function sendEmail(name, surname, telNo, email, availableHours){
-
+function sendEmail(name, surname, telNo, email, availableHours) {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.office365.com',  // Outlook SMTP server
-        port: 587,                  // Port for TLS
-        secure: false,              // true for 465, false for other ports
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
         auth: {
-            user: process.env.EMAIL_USER,     // Access email from environment variable
+            user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
         }
-      });
-      // Burada kayıtlı eposta hesabı ve şifresinin admin'den sitede alınıp database'e güvenli bir şekilde kaydedilmesi gerekiyor. 
+    });
 
-      const mailOptions = {
-        from: process.env.EMAIL_USER,   // Access sender email
-        to: process.env.EMAIL_USER,     // Access recipient email
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
         subject: 'Yeni Randevu Talebi',
         text: `Aşağıdaki bilgilere sahip hasta randevu talebinde bulunmuştur:
         \nAd: ${name}
@@ -159,30 +133,36 @@ function sendEmail(name, surname, telNo, email, availableHours){
         \nTelefon: ${telNo}
         \nE-posta: ${email}
         \nAranmasını istediği saatler: ${availableHours}`
-      };
+    };
 
-
-      transporter.sendMail(mailOptions, (error, info) => {
+    transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-          console.log('Email could not be sent:', error);
+            console.log('Email could not be sent:', error);
         } else {
-          console.log('Email sent:', info.response);
+            console.log('Email sent:', info.response);
         }
-     });    
+    });
 }
+
 function isEmailValid(email) {
-  //check if the email is valid
-  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  let isTempMail = false;
-  // if email doesn't have popular domains, it is a temporary mail
-  let popularDomains = ["gmail.com", "hotmail.com", "outlook.com", "yahoo.com", "yandex.com", "protonmail.com", "edu.tr"];
-  
-  let domain = email.split("@")[1]; // get the domain part of the email
-  
-  if(!popularDomains.includes(domain)){ // if the domain is not in the popular domains, it is a temporary mail
-    isTempMail = true;
-  }
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let isTempMail = false;
+    let popularDomains = ["gmail.com", "hotmail.com", "outlook.com", "yahoo.com", "yandex.com", "protonmail.com", "edu.tr"];
 
-  return emailRegex.test(email) && !isTempMail; // if the email is valid and not a temporary mail, return true
+    let domain = email.split("@")[1];
+    if (!popularDomains.includes(domain)) {
+        isTempMail = true;
+    }
+
+    return emailRegex.test(email) && !isTempMail;
 }
 
+//bunu neden koyduk? unit test'e fonksiyonu aktarabilelim diye
+module.exports = {
+  sendVerificationCode, 
+  isEmailValid,
+  sendEmail,
+  server
+};
+
+//module.exports = { server} ;
--- unit_testing.test.js
+++ unit_testing.test.js
@@ -1,28 +1,184 @@
-// verificationCode.test.js
+// const { sendVerificationCode } = require('./appoinment_request.js'); // appoinment_request.js dosyanızın yolunu düzenleyin
+// const nodemailer = require('nodemailer');
+// const sinon = require('sinon'); // Fonksiyon çağrılarını izlemek ve kontrol etmek için sinon kullanabilirsiniz
+// const chai = require('chai'); // Import Chai
 
-import { describe, it } from 'mocha'; // Test çerçevesini içe aktarın
-import { expect } from 'chai'; // Testleri yazmak için chai kullanabilirsiniz
-import { sendVerificationCode } from './appoinment_request.js'; // Modülünüzün yolunu belirtin
 
-// Unit test
-describe('sendVerificationCode', function() {
-  it('should generate a verification code and send an email', function() {
-    // Test için gerekli veriler
-    const name = 'John';
-    const surname = 'Doe';
-    const email = 'test@example.com';
+// describe('sendVerificationCode function', () => {
+//     it('should send a verification email', () => {
+//         // Sahte bir nodemailer transporter oluşturun
+//         const fakeTransporter = {
+//             sendMail: sinon.spy() // Fonksiyonun çağrılarını izlemek için sinon kullanılır
+//         };
 
-    // İşlevi çağırın
-    const result = sendVerificationCode(name, surname, email);
+//         sinon.replace(nodemailer, 'createTransport', sinon.fake.returns(fakeTransporter));
 
-    // Gerçek sonuçları kontrol edin
-    // Örneğin, e-posta gönderme başarılı mı?
-    expect(result).to.equal('Email sent: SUCCESS'); // Başarılı durumu doğrulayın
+//         // sendVerificationCode fonksiyonunu çağırın
+//         sendVerificationCode('John', 'Doe', 'cezzar.ahmet@example.com');
 
-    // E-posta içeriği oluşturuldu mu?
-    // Email içeriğini çıkarın ve expect ile kontrol edebilirsiniz
-    // Örneğin, içerikte beklenen kod var mı?
-    const emailContent = '...'; // E-posta içeriği burada
-    expect(emailContent).to.include('Doğrulama Kodu:'); // Beklenen içeriği kontrol edin
-  });
+//         // Sahte transporter üzerinde sendMail fonksiyonunun çağrılarını kontrol edin
+//         sinon.assert.calledOnce(fakeTransporter.sendMail);
+//         const callArgs = fakeTransporter.sendMail.getCall(0).args[0]; // İlk çağrının argümanlarını alın
+
+//         // Doğru e-posta adresine doğru verileri gönderip göndermediğini kontrol edin
+//         expect(callArgs.from).to.equal(process.env.EMAIL_USER);
+//         expect(callArgs.to).to.equal('cezzar.ahmet@example.com');
+//         expect(callArgs.subject).to.equal('Doğrulama Kodu');
+
+//         // Diğer kontrol ve testleri burada ekleyebilirsiniz
+
+//         // Son olarak sinon değişikliklerini geri alın
+//         sinon.restore();
+//     });
+// });
+
+
+const { isEmailValid } = require('./appoinment_request.js');
+const request = require('supertest');
+const express = require('express');
+const app = express();
+const { server } = require('./appoinment_request.js');
+const { sendVerificationCode } = require('./appoinment_request.js');
+const { sendEmail } = require('./appoinment_request.js');
+
+
+const sinon = require('sinon');
+const nodemailer = require('nodemailer');
+
+
+
+// app.post('/submit', (req, res) => {
+//     // The function from appointment_request.js goes here
+// });
+
+// describe('POST /submit', () => {
+//     it('responds with json', async () => {
+//         const req = { body: { name: 'John', surname: 'Doe', telNo: '1234567890', email: 'cezzar.ahmet@example.com', availableHours: '9-5' } };
+//         const res = { render: jest.fn() };
+
+//         await request(app)
+//             .post('/submit')
+//             .send(req.body)
+//             .expect('Content-Type', /json/)
+//             .expect(200);
+
+//         expect(res.render).not.toHaveBeenCalledWith('index.ejs', { error: 'error' });
+//     });
+// });
+
+describe('isEmailValid function', () => {
+    it('validates correct email: cezzar.ahmet@outlook.com', () => {
+        expect(isEmailValid('cezzar.ahmet@outlook.com')).toBe(true);
+    });
+
+    it('validates correct email: cezzar.ahmet@gmail.com', () => {
+        expect(isEmailValid('cezzar.ahmet@gmail.com')).toBe(true);
+    });
+
+    it('validates correct email: cezzar.ahmet@hotmail.com', () => {
+        expect(isEmailValid('cezzar.ahmet@hotmail.com')).toBe(true);
+    });
+
+    it('validates correct email: cezzar.ahmet@yahoo.com', () => {
+        expect(isEmailValid('cezzar.ahmet@yahoo.com')).toBe(true);
+    });
+
+    it('invalidates incorrect email: cezzar.ahmet@.com', () => {
+        expect(isEmailValid('cezzar.ahmet@.com')).toBe(false);
+    });
+
+    it('invalidates incorrect email: cezzar.ahmet@example.com', () => {
+        expect(isEmailValid('cezzar.ahmet@example.com')).toBe(false);
+    });
+
+    it('invalidates incorrect email: cezzar.ahmet@example.com', () => {
+        expect(isEmailValid('cezzar.ahmet@example.com')).toBe(false);
+    });
+
+    it('invalidates incorrect email: cezzar.ahmet@aaa', () => {
+        expect(isEmailValid('cezzar.ahmet@aaa')).toBe(false);
+    });
+
+    it('invalidates incorrect email: cezzar.ahmet outlook.com', () => {
+        expect(isEmailValid('cezzar.ahmet outlook.com')).toBe(false);
+    });
+
+    it('invalidates incorrect email: loremipsum', () => {
+        expect(isEmailValid('loremipsum')).toBe(false);
+    });
+});
+
+
+
+
+describe('sendVerificationCode function', () => {
+    it('should send an email with a verification code', () => {
+        // Mock nodemailer's createTransport function
+        const createTransportStub = sinon.stub(nodemailer, 'createTransport');
+        createTransportStub.returns({
+            sendMail: (options, callback) => {
+                // Simulate a successful email sending
+                callback(null, { response: 'Email sent successfully' });
+            }
+        });
+
+        // Stub Math.random to always return 0.5 for predictable verification code
+        const randomStub = sinon.stub(Math, 'random').returns(0.5);
+
+        // Mock the console.log function to avoid printing to the console during the test
+        const consoleLogStub = sinon.stub(console, 'log');
+
+        // Call the function with sample data
+        sendVerificationCode('John', 'Doe', 'cezzar.ahmet@outlook.com');
+        // This function does not control whether email has correct form or not
+
+        // Assertions
+        expect(createTransportStub.calledOnce).toBe(true); // checks if the email transport creation was invoked once.
+        expect(randomStub.calledOnce).toBe(true);  // checks if the random number generation was invoked once.
+        expect(consoleLogStub.calledWith('Email sent:')).toBe(true); // checks if a log statement was executed with the specified message. This line asserts that the consoleLogStub (which is a sinon stub for the console.log function) has been called with the argument 'Email sent:' during the execution of the sendVerificationCode function.
+
+        // Clean up the stubs
+        createTransportStub.restore();  
+        randomStub.restore();  
+        consoleLogStub.restore();  
+    });
+});
+
+
+
+describe('sendEmail function', () => {
+    it('should send an email to dentist with appointment information', () => {
+        // Mock nodemailer's createTransport function
+        const createTransportStub = sinon.stub(nodemailer, 'createTransport');
+        createTransportStub.returns({
+            sendMail: (options, callback) => {
+                // Simulate a successful email sending
+                callback(null, { response: 'Email sent successfully' });
+            }
+        });
+
+        // Mock console.log to capture log messages
+        const consoleLogStub = sinon.stub(console, 'log');
+
+        // Call the function with sample data
+        sendEmail('Cezzar', 'Ahmet', '123456789', 'cezzar.ahmet@gmail.com', 'Monday, 9-11 AM');
+
+        // Assertions
+        expect(createTransportStub.calledOnce).toBe(true);  // checks if the email transport creation was invoked once.
+        expect(consoleLogStub.calledOnce).toBe(true);
+        expect(consoleLogStub.calledWith('Email sent:')).toBe(true);  // checks if a log statement was executed with the specified message. This line asserts that the consoleLogStub (which is a sinon stub for the console.log function) has been called with the argument 'Email sent:' during the execution of the sendVerificationCode function.
+
+        // Clean up the stubs
+        createTransportStub.restore();
+        consoleLogStub.restore();  
+    });
+});
+
+
+
+
+
+
+afterAll(done => {
+    server.close(done);
 });

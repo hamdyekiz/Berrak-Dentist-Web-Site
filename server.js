5c21af4d3af3f369ab1ed38f6b91018442082035
Omer <kurtlarsen31@gmail.com>
2023-12-11 15:43:56 +0300

    appoinment_request.js file has been added to routers directory

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,26 @@ const admin_panelRoutes = require('./routes/admin_panel.js');
 const loginRoutes = require('./routes/login.js');
 
 // Use the route handlers
+app.use('/admin_panel', admin_panelRoutes); // change '/admin_panel.js' to '/admin_panel'
+app.use('/login', loginRoutes); // change '/login.js' to '/login'
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+*/
+
+const express = require('express');
+const app = express();
+
+const appoinment_requestRoutes = require('./routes/appoinment_request.js');
+
+const admin_panelRoutes = require('./routes/admin_panel.js');
+const loginRoutes = require('./routes/login.js');
+
+// Use the route handlers
+app.use('/appoinment_request', appoinment_requestRoutes);
 app.use('/admin_panel', admin_panelRoutes); // change '/admin_panel.js' to '/admin_panel'
 app.use('/login', loginRoutes); // change '/login.js' to '/login'
 
